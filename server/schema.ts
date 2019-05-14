e8725842aa93e5b1b0548145b2e4b31f3f9d2e40
Matthew Doto <mdoto@stevens.edu>
2019-05-14 19:30:52 -0400

    Added graphql types for User, Campaign, Update, and Donation. Resolves #1, #3, #4, #5

--- server/schema.ts
+++ server/schema.ts
@@ -2,50 +2,43 @@ import {gql} from 'apollo-server-express';
 
 export const typeDefs = gql`
 type Query {
-  launches: [Launch]!
-  launch(id: ID!): Launch
   me: User
 }
 
 type Mutation {
-  bookTrips(launchIds: [ID]!): TripUpdateResponse!
-  bookTrip(launchId: ID!): TripUpdateResponse!
-  login(email: String): String
-}
-
-type Launch {
-  id: ID!
-  site: String
-  mission: Mission
-  rocket: Rocket
-  isBooked: Boolean!
-}
-
-type Rocket {
-  id: ID!
-  name: String
-  type: String
 }
 
 type User {
   id: ID!
-  email: String!
-  trips: [Launch]!
+  name: String!
+  doctor: Boolean!
+  bio: String
+  picture: String
 }
 
-type Mission {
-  name: String
-  missionPatch(size: PatchSize): String
+type Campaign {
+  id: ID!
+  description: String!
+  creator: User!
+  goal: Int!
+  recurring: Boolean!
+  doctor: User
+  wantsApproval: Boolean
+  updates: [ID]
+  donations: [ID]
 }
 
-enum PatchSize {
-  SMALL
-  LARGE
+type Update {
+  id: ID!
+  user: ID!
+  comment: String!
+  date: String!
 }
 
-type TripUpdateResponse {
-  success: Boolean!
-  message: String
-  launches: [Launch]
+type Donation {
+  id: ID!
+  user: ID!
+  campaign: ID!
+  amount: Int!
 }
 `;
