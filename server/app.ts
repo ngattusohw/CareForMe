355a1b8ac1a9da140018de0481aa9a96a5cf6bab
ngattusohw <ngattusohw@gmail.com>
2019-05-17 13:02:48 -0400

    Minor fixed to get server to compile

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,20 @@
 import express = require('express');
-import {ApolloServer} from 'apollo-server-express';
-import {createServer} from 'http';
-import {typeDefs} from './schema';
-import {resolvers} from './resolver';
+import { ApolloServer } from 'apollo-server-express';
+import { createServer } from 'http';
+import { typeDefs } from './schema';
+import { resolvers } from './resolver';
 
 const PORT = 5555;
 const app = express();
 
 const path = '/api/graphql';
-const apolloserver = new ApolloServer({typeDefs, resolvers});
-apolloserver.applyMiddleware({app, path});
+const apolloserver = new ApolloServer({
+	typeDefs,
+	resolvers: resolvers as any,
+});
+apolloserver.applyMiddleware({ app, path });
 
 const server = createServer(app);
-server.listen(PORT, ()=>{
-    console.log("Server is listening on port "+PORT);
+server.listen(PORT, () => {
+	console.log('Server is listening on port ' + PORT);
 });
--- server/schema.ts
+++ server/schema.ts
@@ -1,65 +1,96 @@
-import {gql} from 'apollo-server-express';
+import { gql } from 'apollo-server-express';
 
 export const typeDefs = gql`
-type Query {
-  getUser(id: ID): User
-  getDoctors: [User]!
-  getCampaigns: [Campaign]!
-  getCampaignsFiltered(filter: Campaign): [Campaign]!
-  getUpdates(campaignid: ID): [Update]!
-  getDonationsByUser(userid: ID): [Donation]!
-  getDonationsByCampaign(campaignid: ID): [Donation]!
-  getDonation(id: ID): Donation
-  getUpdatesByCampaign(campaignid: ID): [Update]!
-  me: User
-}
+	type User {
+		id: ID!
+		name: String!
+		doctor: Boolean!
+		date: String!
+		bio: String
+		picture: String
+	}
 
-type Mutation {
-  donate(campaignid: ID, amount: Int): Donation
-  update(campaignid: ID, update: Update): Update
-  createCampaign(description: String, creatorid: ID, goal: Int, recurring: Boolean, wantsApproval: Boolean): Campaign
-  createUser(name: String, doctor: Boolean, bio: String, picture: String): User
-  updateUser(userid: ID, user: User): User
-  deleteUser(id: ID): Boolean
-  deleteCampaign(id: ID): Boolean
-  deleteDonationPledge(id: ID): Boolean
-}
+	input UserInput {
+		name: String
+		doctor: Boolean
+		date: String
+		bio: String
+		picture: String
+	}
 
-type User {
-  id: ID!
-  name: String!
-  doctor: Boolean!
-  date: String!
-  bio: String
-  picture: String
-}
+	type Campaign {
+		id: ID!
+		description: String!
+		creatorid: ID!
+		goal: Int!
+		recurring: Boolean!
+		date: String!
+		doctorid: ID
+		wantsApproval: Boolean
+		updateIds: [ID]
+		donationIds: [ID]
+	}
 
-type Campaign {
-  id: ID!
-  description: String!
-  creatorid: ID!
-  goal: Int!
-  recurring: Boolean!
-  date: String!
-  doctorid: ID
-  wantsApproval: Boolean
-  updateIds: [ID]
-  donationIds: [ID]
-}
+	input CampaignInput {
+		description: String
+		creatorid: ID
+		goal: Int
+		recurring: Boolean
+		date: String
+		doctorid: ID
+		wantsApproval: Boolean
+	}
 
-type Update {
-  id: ID!
-  userId: ID!
-  campaignId: ID!
-  comment: String!
-  date: String!
-}
+	type Update {
+		id: ID!
+		userId: ID!
+		campaignId: ID!
+		comment: String!
+		date: String!
+	}
 
-type Donation {
-  id: ID!
-  userId: ID!
-  campaignId: ID!
-  amount: Int!
-  date: String!
-}
+	input UpdateInput {
+		comment: String
+		userId: ID
+		campaignId: ID
+		date: String
+	}
+
+	type Donation {
+		id: ID!
+		userId: ID!
+		campaignId: ID!
+		amount: Int!
+		date: String!
+	}
+
+	type Query {
+		getUser(id: ID): User
+		getDoctors: [User]!
+		getCampaigns: [Campaign]!
+		getCampaignsFiltered(filter: CampaignInput): [Campaign]!
+		getUpdates(campaignid: ID): [Update]!
+		getDonationsByUser(userid: ID): [Donation]!
+		getDonationsByCampaign(campaignid: ID): [Donation]!
+		getDonation(id: ID): Donation
+		getUpdatesByCampaign(campaignid: ID): [Update]!
+		me: User
+	}
+
+	type Mutation {
+		donate(campaignid: ID, amount: Int): Donation
+		update(campaignid: ID, update: UpdateInput): Update
+		createCampaign(
+			description: String
+			creatorid: ID
+			goal: Int
+			recurring: Boolean
+			wantsApproval: Boolean
+		): Campaign
+		createUser(name: String, doctor: Boolean, bio: String, picture: String): User
+		updateUser(userid: ID, user: UserInput): User
+		deleteUser(id: ID): Boolean
+		deleteCampaign(id: ID): Boolean
+		deleteDonationPledge(id: ID): Boolean
+	}
 `;
