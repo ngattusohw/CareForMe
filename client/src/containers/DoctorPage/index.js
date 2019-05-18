ec0ebe42d854fb6a96b5febd590d40c82dde22dd
ngattusohw <ngattusohw@gmail.com>
2019-05-18 04:51:25 -0400

    Adding more structure to DoctorPage

--- client/src/containers/DoctorPage/index.js
+++ client/src/containers/DoctorPage/index.js
@@ -1,10 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, Button, Card, Segment, Feed, Icon, Header } from 'semantic-ui-react';
+import styles from './DoctorPage.module.css';
+
+const submit = (status, cId) => {
+	if (status) {
+		//approve it
+	} else {
+		//deny
+	}
+};
+
+const temporary_queue = [
+	{
+		name: 'Jake Collier',
+		description: 'Hi',
+		amount: '100',
+		campaignId: '12313',
+	},
+];
 
 const DoctorPage = () => {
 	const [approvalQueue, setApprovalQueue] = useState([]);
 
-	useEffect(() => {}, []);
+	useEffect(() => {
+		setApprovalQueue(temporary_queue);
+	}, [temporary_queue]);
 
 	return (
 		<div>
@@ -17,28 +37,43 @@ const DoctorPage = () => {
 				Welcome, Dr.
 			</Header>
 			<Segment>
-				<Feed>
-					<Card>
-						<Card.Content>
-							{/* <Image floated="right" size="mini" src="/images/avatar/large/steve.jpg" /> */}
-							<Card.Header>Steve Sanders</Card.Header>
-							<Card.Meta>Friends of Elliot</Card.Meta>
-							<Card.Description>
-								Steve wants to add you to the group <strong>best friends</strong>
-							</Card.Description>
-						</Card.Content>
-						<Card.Content extra>
-							<div className="ui two buttons">
-								<Button basic color="green">
-									Approve
-								</Button>
-								<Button basic color="red">
-									Decline
-								</Button>
-							</div>
-						</Card.Content>
-					</Card>
-				</Feed>
+				<div className={styles.feedContainer}>
+					<Feed>
+						{approvalQueue.map(c => (
+							<Card key={c.name}>
+								<Card.Content>
+									{/* <Image floated="right" size="mini" src="/images/avatar/large/steve.jpg" /> */}
+									<Card.Header>{c.name}</Card.Header>
+									<Card.Meta>{c.amount}</Card.Meta>
+									<Card.Description>{c.description}</Card.Description>
+								</Card.Content>
+								<Card.Content extra>
+									<div className="ui two buttons">
+										<Button
+											basic
+											color="green"
+											onClick={() => {
+												submit(true, c.campaignId);
+											}}
+										>
+											Approve
+										</Button>
+										<Button
+											basic
+											color="red"
+											onClick={() => {
+												submit(false, c.campaignId);
+											}}
+										>
+											Decline
+										</Button>
+									</div>
+								</Card.Content>
+							</Card>
+						))}
+					</Feed>
+					<Button fluid onClick={() => {}} content="Load more" />
+				</div>
 			</Segment>
 		</div>
 	);
