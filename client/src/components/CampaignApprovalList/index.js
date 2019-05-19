e90785e1bbcb478c4550aea2589b5debbdbe1355
ngattusohw <ngattusohw@gmail.com>
2019-05-19 15:04:12 -0400

    DOCTOR APPROVAL WORKS

--- client/src/components/CampaignApprovalList/index.js
+++ client/src/components/CampaignApprovalList/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import { Button, Card } from 'semantic-ui-react';
 import styles from './CampaignApprovalList.module.css';
 
@@ -20,6 +20,14 @@ const GET_CAMPAIGNS = gql`
 	}
 `;
 
+const APPROVE_CAMPAIGN = gql`
+	mutation ApproveCampaign($campaignid: ID!) {
+		approveCampaign(id: $campaignid) {
+			id
+		}
+	}
+`;
+
 const CampaignApprovalList = () => (
 	<Query query={GET_CAMPAIGNS}>
 		{({ loading, error, data }) => {
@@ -30,36 +38,47 @@ const CampaignApprovalList = () => (
 					{data.getCampaignsFiltered.length === 0
 						? 'No campaigns to approve!'
 						: data.getCampaignsFiltered.map(c => (
-								<Card key={c.id}>
-									<Card.Content>
-										{/* <Image floated="right" size="mini" src="/images/avatar/large/steve.jpg" /> */}
-										<Card.Header>{c.title}</Card.Header>
-										<Card.Meta>{`${c.creatorName} wants to raise $${c.goal}`}</Card.Meta>
-										<Card.Description>{c.description}</Card.Description>
-									</Card.Content>
-									<Card.Content extra>
-										<div className="ui two buttons">
-											<Button
-												basic
-												color="green"
-												onClick={() => {
-													submit(true, c.campaignId);
-												}}
-											>
-												Approve
-											</Button>
-											<Button
-												basic
-												color="red"
-												onClick={() => {
-													submit(false, c.campaignId);
-												}}
-											>
-												Decline
-											</Button>
-										</div>
-									</Card.Content>
-								</Card>
+								<Mutation mutation={APPROVE_CAMPAIGN}>
+									{(approveCampaign, { data, error, loading }) => {
+										if (loading) return 'Loading...';
+										if (error) return `Error! ${error.message}`;
+										return (
+											<Card key={c.id}>
+												<Card.Content>
+													{/* <Image floated="right" size="mini" src="/images/avatar/large/steve.jpg" /> */}
+													<Card.Header>{c.title}</Card.Header>
+													<Card.Meta>{`${c.creatorName} wants to raise $${
+														c.goal
+													}`}</Card.Meta>
+													<Card.Description>{c.description}</Card.Description>
+												</Card.Content>
+												<Card.Content extra>
+													<div className="ui two buttons">
+														<Button
+															basic
+															color="green"
+															onClick={() => {
+																console.log(c.id);
+																approveCampaign({ variables: { campaignid: c.id } });
+															}}
+														>
+															Approve
+														</Button>
+														<Button
+															basic
+															color="red"
+															onClick={() => {
+																submit(false, c.campaignId);
+															}}
+														>
+															Decline
+														</Button>
+													</div>
+												</Card.Content>
+											</Card>
+										);
+									}}
+								</Mutation>
 						  ))}
 				</div>
 			);
