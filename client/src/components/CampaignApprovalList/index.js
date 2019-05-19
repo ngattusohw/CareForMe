d8e106e2ff271a06cb4920c5a15e9cc1b86947b0
ngattusohw <ngattusohw@gmail.com>
2019-05-19 14:51:00 -0400

    Fixing doctor approval queue

--- client/src/components/CampaignApprovalList/index.js
+++ client/src/components/CampaignApprovalList/index.js
@@ -10,7 +10,7 @@ const submit = () => {
 
 const GET_CAMPAIGNS = gql`
 	{
-		getCampaignsFiltered(filter: { wantsApproval: true }) {
+		getCampaignsFiltered(filter: { wantsApproval: true, hasApproval: false }) {
 			id
 			description
 			goal
@@ -34,7 +34,7 @@ const CampaignApprovalList = () => (
 									<Card.Content>
 										{/* <Image floated="right" size="mini" src="/images/avatar/large/steve.jpg" /> */}
 										<Card.Header>{c.title}</Card.Header>
-										<Card.Meta>{`${c.creatorName} wants to raise ${c.goal}`}</Card.Meta>
+										<Card.Meta>{`${c.creatorName} wants to raise $${c.goal}`}</Card.Meta>
 										<Card.Description>{c.description}</Card.Description>
 									</Card.Content>
 									<Card.Content extra>
